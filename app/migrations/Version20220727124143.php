df0c40b5c9f7720199b707f6f98d7b85b3a76a44
Marcin Szostak <marcin.szostak@boldare.com>
2022-08-22 15:18:56 +0200

    fix: adding role assigning for migration

--- app/migrations/Version20220727124143.php
+++ app/migrations/Version20220727124143.php
@@ -19,7 +19,7 @@ final class Version20220727124143 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('INSERT INTO `users` SET `username`="johndoe", `password`="dy9mTWFQQ3V5bEE2d0I4Rw$0k2q7RVUjHTX0Mk4SLrKXEUITW7gDoElPMfeAn7+lPU"');
+        $this->addSql('INSERT INTO `users` SET `username`="johndoe", `password`="dy9mTWFQQ3V5bEE2d0I4Rw$0k2q7RVUjHTX0Mk4SLrKXEUITW7gDoElPMfeAn7+lPU", `roles`=\'["ROLE_ADMIN"]\'');
     }
 
     public function down(Schema $schema): void
